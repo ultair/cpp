b22586c4d4474eaf5eb110512f54978e9206ca34
Mark McWalter <mark@mmw.ie>
2022-05-14 18:24:36 +0100

    Latest changes

--- containers/deque.cpp
+++ containers/deque.cpp
@@ -29,10 +29,10 @@ public:
 
     size_t size();
 
-    inline constexpr size_t GetNodeSize(size_t elSize)
+    inline constexpr size_t GetNodeSize()
     {
-    	return ( elSize < blockSize_
-                 ? size_t(blockSize_ / elSize) : size_t(1));
+    	return ( sizeof(T) < blockSize_
+                 ? size_t(blockSize_ / sizeof(T)) : size_t(1));
     }
 
 private:
@@ -53,9 +53,9 @@ private:
 
     T** const blockMap_{nullptr};
 
-    int blockBegin_{3};
+    int blockBegin_ = mapSize_ / 2;
 
-    int blockEnd_{3};
+    int blockEnd_ = mapSize_ / 2;
 
     T* front_{nullptr};
 
@@ -87,13 +87,85 @@ MyDeque<T>::~MyDeque()
 template <typename T>
 T* MyDeque<T>::AllocateNode()
 {
-    return reinterpret_cast<T*>(calloc(GetNodeSize(sizeof(T)), sizeof(T)));
+    std::cout << "Allocating a node with size of " << GetNodeSize() << std::endl;
+
+    return reinterpret_cast<T*>(calloc(GetNodeSize(), sizeof(T)));
 }
 
 template <typename T>
 void MyDeque<T>::push_front(const T& item)
 {
+    ExtendFront();
 
+    *front_ = item;
+}
+
+template <typename T>
+void MyDeque<T>::ExtendFront()
+{
+    if(front_)
+    {
+        // Are we at the start of the head node?
+        if( blockMap_[blockBegin_] == front_ )
+        {
+            if( blockBegin_ == 0 )
+            {
+                std::cout << "Map size exceeded. Exit for now" << std::endl;
+                exit(1);
+            }
+            else
+            {
+                --blockBegin_;
+
+                // Allocate a new head node
+                blockMap_[blockBegin_] = AllocateNode();
+
+                front_ = blockMap_[blockBegin_] + (GetNodeSize() - 1);
+            }
+        }
+        else
+        {
+            --front_;
+        }
+    }
+    else
+    {
+        --blockBegin_;
+
+        // Allocate a new head node
+        blockMap_[blockBegin_] = AllocateNode();
+
+        front_ = blockMap_[blockBegin_] + (GetNodeSize() - 1);
+    }
+}
+
+template <typename T>
+void MyDeque<T>::RegressFront()
+{
+    if(front_)
+    {
+        // Are we at the end of the head node?
+        if( (blockMap_[blockBegin_] + (GetNodeSize() - 1)) == front_ )
+        {
+            if( blockBegin_ < blockEnd_ )
+            {
+                // Deallocate the node
+                free(blockMap_[blockBegin_]);
+
+                ++blockBegin_;
+
+                front_ = blockMap_[blockBegin_];
+            }
+            else
+            {
+                front_ = nullptr;
+            }
+        }
+        else
+        {
+            ++front_;
+        }
+    }
 }
 
 template <typename T>
@@ -102,6 +174,17 @@ void MyDeque<T>::push_back(const T& item)
     ExtendBack();
 
     *back_ = item;
+}
+
+template <typename T>
+void MyDeque<T>::pop_front()
+{
+    if(front_)
+    {
+        std::destroy_at(front_);
+
+        RegressFront();
+    }
 }
 
 template <typename T>
@@ -121,7 +204,7 @@ void MyDeque<T>::ExtendBack()
     if(back_)
     {
         // Are we at the end of the tail node?
-        if( (blockMap_[blockEnd_] + (GetNodeSize(sizeof(T)) - 1)) == back_)
+        if( (blockMap_[blockEnd_] + (GetNodeSize() - 1)) == back_ )
         {
             if( (blockEnd_+1) >= mapSize_ )
             {
@@ -164,7 +247,7 @@ void MyDeque<T>::RegressBack()
 
                 --blockEnd_;
 
-                back_ = blockMap_[blockEnd_] + (GetNodeSize(sizeof(T)) - 1);
+                back_ = blockMap_[blockEnd_] + (GetNodeSize() - 1);
             }
             else
             {
@@ -187,7 +270,7 @@ size_t MyDeque<T>::size()
     }
     else if(back_)
     {
-        size_t nodeElementCount = (blockEnd_ - blockBegin_) * GetNodeSize(sizeof(T));
+        size_t nodeElementCount = (blockEnd_ - blockBegin_) * GetNodeSize();
 
         return 1 + (back_ - blockMap_[blockEnd_]) + nodeElementCount;
     }
@@ -198,31 +281,62 @@ size_t MyDeque<T>::size()
 template <typename T>
 T& MyDeque<T>::at(size_t pos)
 {
+    //std::cout << "Search for " << pos << std::endl;
+
+    int nodeM{0};
+
     if(front_)
     {
-        // PLACEHOLDER
-        return blockMap_[0][0];
+        int headLastPos = (blockMap_[blockBegin_] + GetNodeSize() - 1) - front_;
+
+        if(pos <= headLastPos)
+        {
+            // The element is in the head node
+            return *(front_ + pos);
+        }
+        else
+        {
+            // The element will be found in the second node onwards
+            pos -= (headLastPos + 1);
+
+            nodeM = 1;
+        }
     }
-    else if(back_)
+
+    int nodeIndex = (pos / GetNodeSize()) + nodeM;
+
+    int nodeOffset = pos;
+
+    if(nodeOffset >= GetNodeSize())
     {
-        int nodeIndex = pos / GetNodeSize(sizeof(T));
-
-        int nodeOffset = pos - (GetNodeSize(sizeof(T)) * nodeIndex);
-
-        return blockMap_[blockBegin_ + nodeIndex][nodeOffset];
+        nodeOffset = pos % GetNodeSize();
     }
 
-    // PLACEHOLDER
-    return blockMap_[0][0];
+/*    std::cout << "Pos: " << pos
+              << " nodeM: " << nodeM
+              << " nodeIndex: " << nodeIndex
+              << " nodeOffset: " << nodeOffset
+              << std::endl;*/
+
+    return blockMap_[blockBegin_ + nodeIndex][nodeOffset];
 }
 
 int main(int argc, char** argv)
 {
     MyDeque<int> deque;
 
-    std::cout << "Size: " << deque.size() << std::endl;
+    for(int i=1; i<=260; i++)
+    {
+        std::cout << "Push front: " << i << std::endl;
+        deque.push_front(i);
+    }
 
-    deque.push_back(100);
+    for(int i=0; i<260; i++)
+    {
+        int n = deque.at(i);
+
+        std::cout << "at " << i << ": " << n << std::endl;
+    }
 
     return 0;
 }
